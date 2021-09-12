fa5a47d73450063e0b95267b6554b0f27117f5e1
ilyasbabu <ilyasbabuvp@gmail.com>
2021-09-12 15:03:16 +0530

    almost finshed yaygit status!   layout re designed after learning much more. included heading for window and much more

--- calculator.py
+++ calculator.py
@@ -1,41 +1,58 @@
 from tkinter import *
 
 window=Tk()
-window.geometry('500x500')
-
-textDisplay=Text(window,height=4,width=40)
-
-button9=Button(window,text=9,width=8,height=2)
-button8=Button(window,text=8,width=8,height=2)
-button7=Button(window,text=7,width=8,height=2)
-plusbutton=Button(window,text='+',width=8,height=2)
-button6=Button(window,text=6,width=8,height=2)
-button5=Button(window,text=5,width=8,height=2)
-button4=Button(window,text=4,width=8,height=2)
-minusbutton=Button(window,text='+',width=8,height=2)
-button3=Button(window,text=3,width=8,height=2)
-button2=Button(window,text=2,width=8,height=2)
-button1=Button(window,text=1,width=8,height=2)
-mulbutton=Button(window,text='+',width=8,height=2)
-button0=Button(window,text=0,width=16,height=2)
-equalbutton=Button(window,text="=",width=16,height=2)
+window.geometry('312x500')
+window.resizable(0,0)
+window.title("Calculator")
 
 
-textDisplay.grid(row=0,column=0,columnspan=3)
-button9.grid(row=1,column=0)
-button8.grid(row=1,column=1)
-button7.grid(row=1,column=2)
-button6.grid(row=2,column=0)
-button5.grid(row=2,column=1)
-button4.grid(row=2,column=2)
-button3.grid(row=3,column=0)
-button2.grid(row=3,column=1)
-button1.grid(row=3,column=2)
-button0.grid(row=4,column=0,columnspan=2)
-equalbutton.grid(row=4,column=2,columnspan=2)
-plusbutton.grid(row=1,column=3)
-minusbutton.grid(row=1,column=3)
-mulbutton.grid(row=1,column=3)
+
+def btnClick(values):
+    global btnValue
+    btnValue= btnValue +str(values)
+    inputText.set(btnValue)
+
+def clrBtn():
+    global btnValue
+    btnValue=""
+    inputText.set(btnValue)
+
+def eqlbtn():
+    global btnValue
+    result = str(eval(btnValue)) # 'eval':This function is used to evaluates the string expression directly
+    inputText.set(result)
+    btnValue = ""
+
+btnValue=""
+
+inputText=StringVar()
+
+inputFrame=Frame(window,width=312, height=50, bd=0, highlightbackground="black", highlightcolor="black", highlightthickness=2)
+inputFrame.pack(side=TOP)
+
+inputField = Entry(inputFrame, font=('arial', 18, 'bold'), textvariable=inputText, width=50, bg="#eee", bd=0, justify=RIGHT)
+inputField.grid(row=0,column=0)
+inputField.pack(ipady=10)
+
+btnsFrame=Frame(window,width=312,height=272.5)
+btnsFrame.pack()
+
+button9 = Button(btnsFrame, text = "9", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btnClick(9)).grid(row = 1, column = 0, padx = 1, pady = 1)
+button8=Button(btnsFrame, text = "8", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btnClick(8)).grid(row = 1, column = 1, padx = 1, pady = 1)
+button7=Button(btnsFrame, text = "7", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btnClick(7)).grid(row = 1, column = 2, padx = 1, pady = 1)
+button6=Button(btnsFrame, text = "6", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btnClick(6)).grid(row = 2, column = 0, padx = 1, pady = 1)
+button5=Button(btnsFrame, text = "5", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btnClick(5)).grid(row = 2, column = 1, padx = 1, pady = 1)
+button4=Button(btnsFrame, text = "4", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btnClick(4)).grid(row = 2, column = 2, padx = 1, pady = 1)
+button3=Button(btnsFrame, text = "3", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btnClick(3)).grid(row = 3, column = 0, padx = 1, pady = 1)
+button2=Button(btnsFrame, text = "2", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btnClick(2)).grid(row = 3, column = 1, padx = 1, pady = 1)
+button1=Button(btnsFrame, text = "1", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btnClick(1)).grid(row = 3, column = 2, padx = 1, pady = 1)
+button0=Button(btnsFrame, text = "0", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btnClick(0)).grid(row = 4, column = 1, padx = 1, pady = 1)
+buttonplus=Button(btnsFrame, text = "+", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btnClick('+')).grid(row = 1, column = 3, padx = 1, pady = 1)
+buttonminus=Button(btnsFrame, text = "-", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btnClick('-')).grid(row = 2, column = 3, padx = 1, pady = 1)
+buttondivide=Button(btnsFrame, text = "/", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btnClick('/')).grid(row = 3, column = 3, padx = 1, pady = 1)
+buttonmutiply=Button(btnsFrame, text = "x", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btnClick('*')).grid(row = 4, column = 3, padx = 1, pady = 1)
+buttonclear=Button(btnsFrame, text = "C", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: clrBtn()).grid(row = 4, column = 0, padx = 1, pady = 1)
+buttonmequals=Button(btnsFrame, text = "=", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: eqlbtn()).grid(row = 4, column = 2, padx = 1, pady = 1)
 
 
 window.mainloop()
